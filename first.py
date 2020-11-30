dcee3ebee254e2c0f82b5fa25843f5a4c0ed8617
MihirMadhav <74979133+MihirMadhav@users.noreply.github.com>
2020-11-30 11:26:46 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,49 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 6*10+9
 
-
-# In[2]:
 
 
 no = 3
 
 
-# In[3]:
-
 
 no
 
-
-# In[4]:
 
 
 name = "Mihir"
 
 
-# In[5]:
-
 
 name
 
-
-# In[6]:
 
 
 "henlo " + name
 
 
-# In[7]:
-
 
 type(name)
 
-
-# In[8]:
 
 
 type(no)
